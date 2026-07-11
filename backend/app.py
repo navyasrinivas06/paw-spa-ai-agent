364b9f19278d79c8109e454803d29952550498fd
Navya Srinivas <navya@zuventech.com>
2026-07-11 19:55:30 +0530

    Added Render frontend URL to CORS

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://paw-spa-ai-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
